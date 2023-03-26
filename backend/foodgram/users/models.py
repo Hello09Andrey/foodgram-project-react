88a7a8de00ed65def2c3d01179b87781999181c6
Hello09Andrey <104667768+Hello09Andrey@users.noreply.github.com>
2023-03-26 21:00:49 +0300

    model CustomUser

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .validators import validate_username
+
+
+class CustomUser(AbstractUser):
+    """Модель пользователей"""
+
+    email = models.EmailField(
+        'Адрес электронной почты',
+        unique=True
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150,
+        blank=True
+    )
+    username = models.CharField(
+        'Имя пользователя',
+        max_length=150,
+        unique=True,
+        validators=[validate_username]
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    """Модель с подписками"""
+
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="follower",
+        verbose_name='Подписчик',
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="following",
+        verbose_name='Автор'
+    )
+
+    def __str__(self) -> str:
+        return f'Подписчик: {self.user}, автор: {self.author}'
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'user'],
+                name='unique_follower'
+            )
+        ]
