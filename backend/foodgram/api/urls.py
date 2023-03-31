66772d33dd56034bdb2717d260c71fc094493975
Hello09Andrey <104667768+Hello09Andrey@users.noreply.github.com>
2023-03-31 09:17:39 +0300

    flake8

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,13 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (
+from .views import (
     CustomUserViewSet,
     TagViewSet,
     IngredientsViewSet,
     RecipesViewSet
 )
-
 
 router_v1 = DefaultRouter()
 router_v1.register('users', CustomUserViewSet)
